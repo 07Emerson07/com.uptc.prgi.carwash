5d11164bfaef1ece87b0a898f54b6e889c008378
07Emerson07 <92050135+07Emerson07@users.noreply.github.com>
2021-10-06 23:07:42 -0500

    Update and rename src/com/uptc/prgi/carwash/Vehicles.java to src/com/uptc/prgi/lavado de autos/Vehicles.java

---
+++ src/com/uptc/prgi/lavado de autos/Vehicles.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.uptc.prgi.carwash;
+
+/**
+ *
+ * @author Luis Miguel Santiago 202023610
+ * @date 6/10/2021  
+ */
+public class Vehicles {
+    private String licencePlate;
+    private TypeVehicle typeVehicle;
+    private Service serviceOne;
+    private Service serviceTwo;
+    private Service serviceTwo;
+
+    public Vehicle(String licencePlate, TypeVehicle typeVehicle, Service serviceOne, Service serviceTwo, Service serviceTwo) {
+        this.licencePlate = licencePlate;
+        this.typeVehicle = typeVehicle;
+        this.serviceOne = serviceOne;
+        this.serviceTwo = serviceTwo;
+        this.serviceTwo = serviceTwo;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    public void setTypeVehicle(TypeVehicle typeVehicle) {
+        this.typeVehicle = typeVehicle;
+    }
+
+    public void setServiceOne(Service serviceOne) {
+        this.serviceOne = serviceOne;
+    }
+
+    public void setServiceTwo(Service serviceTwo) {
+        this.serviceTwo = serviceTwo;
+    }
+
+    public void setServiceTwo(Service serviceTwo) {
+        this.serviceTwo = serviceTwo;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public TypeVehicle getTypeVehicle() {
+        return typeVehicle;
+    }
+
+    public Service getServiceOne() {
+        return serviceOne;
+    }
+
+    public Service getServiceTwo() {
+        return serviceTwo;
+    }
+
+    public Service getServiceTwo() {
+        return serviceTwo;
+    }
+    
+}
